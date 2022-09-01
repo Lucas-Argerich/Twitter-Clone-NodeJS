c6cf36e2ba463aa07f4ea9e46ccf9ed2824e373f
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-08-31 23:13:04 -0300

    button className fix

--- components/Button/index.js
+++ components/Button/index.js
@@ -3,7 +3,7 @@ import styles from './Button.module.css'
 export default function Button({ children, onClick }) {
   return (
     <>
-      <button class={styles.button} onClick={onClick}>
+      <button className={styles.button} onClick={onClick}>
         {children}
       </button>
     </>
