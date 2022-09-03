c49cdce782bf10e9adfdc5fea92fa6a4adb71a27
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-09-02 23:47:21 -0300

    avatar unused prop del

--- components/Avatar/index.js
+++ components/Avatar/index.js
@@ -1,6 +1,6 @@
 import styles from './Avatar.module.css'
 
-export default function Avatar({ src, alt, text, withText }) {
+export default function Avatar({ src, alt, text }) {
   return (
     <div className={styles.container}>
       <img className={styles.avatar} src={src} alt={alt} title={alt} />
