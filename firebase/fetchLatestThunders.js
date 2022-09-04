df04a1c23340bccacdad98620a782d8c3b5ff3ce
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-09-04 19:06:07 -0300

    function rename timeAgo()

--- firebase/fetchLatestThunders.js
+++ firebase/fetchLatestThunders.js
@@ -1,7 +1,7 @@
 import { collection, getDocs } from 'firebase/firestore'
 import { db } from './client'
 
-const timeSincePost = (milliseconds) => {
+const timeAgo = (milliseconds) => {
   const diffSec = Math.floor((Date.now() - new Date(milliseconds)) / 1000)
   const diffMin = Math.floor((Date.now() - new Date(milliseconds)) / 60000)
   const diffHour = Math.floor((Date.now() - new Date(milliseconds)) / 3.6e6)
@@ -21,8 +21,7 @@ export default async function fetchLatestThunders() {
   const querySnapshot = await getDocs(collection(db, 'thunders'))
   return querySnapshot.docs.map((doc) => {
     const data = doc.data()
-    const milliseconds = data.createdAt.seconds * 1000
-    data.createdAt = timeSincePost(milliseconds)
+    data.createdAt = timeAgo(data.createdAt.seconds * 1000)
     return { id: doc.id, ...data }
   })
 }
