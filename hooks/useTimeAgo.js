e01c66e0cf6a71572ad8fb6059c68bc9c5ab6276
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-09-04 22:15:24 -0300

    useTimeAgo hook improvements

--- hooks/useTimeAgo.js
+++ hooks/useTimeAgo.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 const DATE_UNITS = [
   ['day', 86400],
   ['hour', 3600],
@@ -11,16 +13,31 @@ const getDateDiffs = (timestamp) => {
 
   for (const [unit, secondsInUnit] of DATE_UNITS) {
     if (Math.abs(elapsed) > secondsInUnit || unit === 'second') {
-      const value = Math.floor(elapsed / secondsInUnit)
+      const value = Math.ceil(elapsed / secondsInUnit)
       return { value, unit }
     }
   }
 }
 
 export default function useTimeAgo(timestamp) {
-  const { value, unit } = getDateDiffs(timestamp)
+  const [timeAgo, setTimeAgo] = useState(() => getDateDiffs(timestamp))
+  const { value, unit } = timeAgo
   const rtf = new Intl.RelativeTimeFormat(navigator.language, {
     style: 'short',
   })
+
+  useEffect(() => {
+    const interval =
+      (unit === 'minute' || unit === 'second') &&
+      setInterval(
+        () => {
+          setTimeAgo(getDateDiffs(timestamp))
+          console.log('interval set', value, unit)
+        },
+        unit === 'second' ? 1000 : 60000
+      )
+    return () => interval && clearInterval(interval)
+  }, [unit])
+
   return rtf.format(value, unit)
 }
