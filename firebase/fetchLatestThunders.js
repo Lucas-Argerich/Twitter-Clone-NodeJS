e1b80931488683f63e253069544b2a1e0e31cc10
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-09-04 23:15:29 -0300

    get Thunders ordered

--- firebase/fetchLatestThunders.js
+++ firebase/fetchLatestThunders.js
@@ -1,8 +1,10 @@
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, orderBy, query } from 'firebase/firestore'
 import { db } from './client'
 
 export default async function fetchLatestThunders() {
-  const querySnapshot = await getDocs(collection(db, 'thunders'))
+  const querySnapshot = await getDocs(
+    query(collection(db, 'thunders'), orderBy('createdAt', 'desc'))
+  )
   return querySnapshot.docs.map((doc) => {
     const data = doc.data()
     data.createdAt = +data.createdAt.toDate()
